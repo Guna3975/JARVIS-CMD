5229168af771c4e1612610fddb70ad78e9914ec4
gokulnath2820 <gokulnath0769@gmail.com>
2025-10-02 13:43:22 +0530

    loop added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 os.system("cls")
 
 def wish_me():
+    """Greet user according to current time"""
     hour = datetime.now().hour
     if 0 <= hour < 12:
         greeting = "Good Morning!"
@@ -14,15 +15,33 @@ def wish_me():
     else:
         greeting = "Good Evening!"
     
-    speak(greeting)
     print(greeting)
+    speak(greeting)
+
+def personal_assistant():
+    """Main loop for personal assistant"""
+    wish_me()
+    print("I am your assistant. You can ask me to open apps or chat with me. Type 'bye' to quit.")
+    speak("I am your assistant. You can ask me to open apps or chat with me. Type 'bye' to quit.")
+
+
+    while True:
+        command = input("\nYou: ").strip()
+        
+        if command.lower() == "bye":
+            print("Jarvis: Goodbye! Have a nice day.")
+            speak("Goodbye! Have a nice day.")
+            break
+
+        # Try to open system app
+        response = open_system_app(command)
+        
+        # If app not recognized, respond like a personal assistant
+        if "don't know" in response:
+            response = f"I'm here to help, but I can't open '{command}'. Maybe try another app?"
+
+        print(f"Jarvis: {response}")
+        speak(response)
 
 if __name__ == "__main__":
-    wish_me()
-    
-    # Instead of voice, take text input for now
-    command = input("Which app do you want to open? ")
-    
-    response = open_system_app(command)
-    print(response)
-    speak(response)
+    personal_assistant()

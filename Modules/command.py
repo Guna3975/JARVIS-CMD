45bebaf680b1109dc5730106d5888f9765ec2b93
Guna3975 <guna39perfect75@gmail.com>
2025-10-02 13:28:57 +0530

    Opening apps in System

---
+++ Modules/command.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+
+def open_system_app(command):
+    command = command.lower()
+
+    # Common folders
+    folders = {
+        "documents": os.path.join(os.environ['USERPROFILE'], 'Documents'),
+        "downloads": os.path.join(os.environ['USERPROFILE'], 'Downloads'),
+        "desktop": os.path.join(os.environ['USERPROFILE'], 'Desktop'),
+        "this pc": "C:\\"
+    }
+
+    for key, path in folders.items():
+        if key in command:
+            os.startfile(path)
+            return f"Opening {key}"
+
+    # Start Menu shortcuts
+    start_menu_paths = [
+        os.path.join(os.environ['ProgramData'], "Microsoft", "Windows", "Start Menu", "Programs"),
+        os.path.join(os.environ['APPDATA'], "Microsoft", "Windows", "Start Menu", "Programs")
+    ]
+
+    for path in start_menu_paths:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.lower().endswith(".lnk") and command in file.lower():
+                    shortcut_path = os.path.join(root, file)
+                    subprocess.Popen(['powershell', '-Command', f'Start-Process "{shortcut_path}"'])
+                    return f"Opening {file.replace('.lnk','')}"
+
+    return "Sorry, I cannot find that app"
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from Speech.speak import speak
+from Modules.command import open_system_app
 from datetime import datetime
 
 def wish_me():
-    """Greet user according to current time"""
     hour = datetime.now().hour
-
     if 0 <= hour < 12:
         greeting = "Good Morning!"
     elif 12 <= hour < 18:
@@ -14,7 +13,13 @@ def wish_me():
     
     speak(greeting)
     print(greeting)
-    
 
 if __name__ == "__main__":
     wish_me()
+    
+    # Instead of voice, take text input for now
+    command = input("Which app do you want to open? ")
+    
+    response = open_system_app(command)
+    print(response)
+    speak(response)

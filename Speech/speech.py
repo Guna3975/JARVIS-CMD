ba1392a49b224ab57022c718465ea9f7ad0862fe
Guna3975 <guna39perfect75@gmail.com>
2025-10-02 12:20:39 +0530

    speech updated

--- Speech/speech.py
+++ Speech/speech.py
@@ -2,7 +2,17 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
+# List available voices
+voices = engine.getProperty('voices')
+for i, voice in enumerate(voices):
+    print(i, voice.name, voice.id)
+
+# Set female voice (choose one from the list)
+engine.setProperty('voice', voices[1].id)  # usually index 1 is female on Windows
+
 def speak(text):
     """Convert text to speech"""
     engine.say(text)
     engine.runAndWait()
+
+speak("Hello, How are you ?,fezkdekfs")
---
+++ main.py
@@ -0,0 +1,19 @@
+from speech.speak import speak
+from datetime import datetime
+
+def wish_me():
+    """Greet user according to current time"""
+    hour = datetime.now().hour
+
+    if 0 <= hour < 12:
+        greeting = "Good Morning!"
+    elif 12 <= hour < 18:
+        greeting = "Good Afternoon!"
+    else:
+        greeting = "Good Evening!"
+    
+    speak(greeting)
+    print(greeting)
+
+if __name__ == "__main__":
+    wish_me()
